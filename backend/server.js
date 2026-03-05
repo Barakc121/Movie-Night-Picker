a50c9ec8b0294196e3ad0ed43f6a928c2f69977e
Barak <Barakc121@gmail.com>
2026-03-05 11:37:39 +0200

    b

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,26 @@
-import express  from 'express'
-import fs from 'fs'
-import cors from 'cors'
+import express from "express";
+import fs from "fs";
+import cors from "cors";
 
-const app=express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.get('/seats/:id',(req,res)=>{
-    constmovieId=req.params.id
-    const data=fs.read
+// us3  get נתיב לקבל את הסרטים
+app.get("/movies", (req, res) => {
+  const data = fs.readFileSync("./movies.json", "utf-8");
+  res.json(JSON.parse(data));
+});
+
+// k לפי סרט שאני אבחר
+app.get("/seats/:id", (req, res) => {
+  const movieId = req.params.id;
+  const data = fs.readFileSync("./seats.json", "utf-8");
+const allSeats=JSON.parse(data)
+res.json(allSeats[movieId] || [])
+});
+
+app.listen(5000,()=>{
+    console.log('server is runing on port 5000')
 })
